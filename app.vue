5107bb86d72a74d23f257dec93301ca834103619
nazimfeni <mdnazimuddin77@gmail.com>
2023-09-26 23:00:51 +0600

    updated

--- app.vue
+++ app.vue
@@ -2,4 +2,21 @@
   <div>
    <h1>Hello World</h1>
   </div>
+  <button @click="increment">Increment</button>
+  <p>{{ count }}</p>
+  <button @click="decrement">Decrement</button>
 </template>
+
+<script setup>
+
+const count = ref(0);
+
+
+function increment(){
+  count.value++;
+}
+
+function decrement(){
+  count.value;
+}
+</script>
